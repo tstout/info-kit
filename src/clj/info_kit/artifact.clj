dab20e6e5f203ded3bf2866eaabc68ce5a04f350
Todd Stout <todd.tstout@gmail.com>
2018-03-18 15:08:55 -0500

    support persisting artifacts with tags

---
+++ src/clj/info_kit/artifact.clj
@@ -0,0 +1,10 @@
+(ns info-kit.artifact)
+
+(defn create-artifact [m]
+  ())
+
+(defn update-artifact [])
+
+(defn read-artifact [])
+
+(defn delete-artifact [])
--- src/clj/info_kit/db_io.clj
+++ src/clj/info_kit/db_io.clj
@@ -12,3 +12,37 @@
   (jdbc/with-db-connection
     [conn db-spec]
     (jdbc/query conn ["select id, name, count from tags where name = ?" tag-name])))
+
+(defn insert-artifact
+  [m]
+  (let [{:keys [name body conn]} m]
+    (->>
+      (jdbc/insert! conn :artifacts {:body body :name name})
+      first
+      ((keyword "scope_identity()")))))
+
+(defn assoc-tags [conn tags artifact-id]
+  (doseq [t tags]
+    (jdbc/insert!
+      conn
+      :artifact_tags
+      {:artifact_id artifact-id
+       :tag_id      t})
+    (jdbc/execute! conn ["UPDATE tags SET count = count + 1 WHERE id = ?" t]))
+  artifact-id)
+
+(defn new-artifact
+  "Create a new artifact based on the specified map.
+  Returns the synthetic key of the freshly-minted artifact.
+  Expected shape of the map defining the artifact:
+  {:db-spec {map of database spec}
+   :body    string defining body of artifact
+   :name    string defining name of artifact>
+   :tags    [vector of tag ids to associate with the artifact]}"
+  [m]
+  (let [{:keys [db-spec tags]} m]
+    (jdbc/with-db-transaction
+      [conn db-spec]
+      (->>
+        (insert-artifact (merge m {:conn conn}))
+        (assoc-tags conn tags)))))
