87c3e93f64af5ea55e72ba043bb593a0b6d81da2
Todd Stout <todd.tstout@gmail.com>
2019-03-03 20:36:23 -0600

    basic side-nav stub

--- src/cljs/info_kit/core.cljs
+++ src/cljs/info_kit/core.cljs
@@ -1,32 +1,62 @@
 (ns info-kit.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [secretary.core :as secretary :include-macros true]
+            [accountant.core :as accountant]))
 
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to info-kit"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+;(defn home-page []
+;  [:div [:h2 "Welcome to info-kit"]
+;   [:div [:a {:href "/about"} "go to about page"]]])
 
 (defn about-page []
-  [:div [:h2 "About info-kit"]
-   [:div [:a {:href "/"} "go to the home page"]]])
+  [:div [:h3 "Ad-hoc stash of information"]
+   [:p "TODO - add version information"]])
+
+(defn reminder-page []
+  [:div [:h3 "Reminders"]])
+
+(defn logs-page []
+  [:div [:h3 "Logs"]])
+
+(defn reporting-page []
+  [:div [:h3 "Reporting"]])
+
+(defn tags-page []
+  [:div [:h3 "Tags"]])
+
 
 ;; -------------------------
 ;; Routes
 
-(defonce page (atom #'home-page))
+(defonce page (atom #'tags-page))
 
 (defn current-page []
-  [:div [@page]])
+  [:div
+   [:div {:class :sidenav}
+    [:a {:href "/"} "Tags"]
+    [:a {:href "/reminders"} "Reminders"]
+    [:a {:href "/logs"} "Logs"]
+    [:a {:href "/reporting"} "Reporting"]
+    [:a {:href "/about"} "About"]]
+   [:div {:class :main}
+    [@page]]])
 
 (secretary/defroute "/" []
-  (reset! page #'home-page))
+                    (reset! page #'tags-page))
 
 (secretary/defroute "/about" []
-  (reset! page #'about-page))
+                    (reset! page #'about-page))
+
+(secretary/defroute "/logs" []
+                    (reset! page #'logs-page))
+
+(secretary/defroute "/reporting" []
+                    (reset! page #'reporting-page))
+
+(secretary/defroute "/reminders" []
+                    (reset! page #'reminder-page))
 
 ;; -------------------------
 ;; Initialize app
--- src/cljs/info_kit/state.cljs
+++ src/cljs/info_kit/state.cljs
@@ -3,7 +3,6 @@
             [cljs.core.async :refer [<! put! pub sub chan <! >! timeout close!]])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
-
 (def app-state (reagent/atom {:env :prod}))
 
 (defn update-state [key val]
